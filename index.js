d1759b0d67cd0e60e8355ceedf57f3a79f290e7d
Brook Zeisse <b@zeisse.com>
2018-02-19 22:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var animal = 'dog'
 
 function myAnimal() {
     var animal ='dog'
-    return animal
-}
-
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
@@ -12,7 +9,17 @@ function yourAnimal() {
   var animal = cat;
   return animal;
   
+}    return animal
 }
+
+//function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+//  var animal = cat;
+//  return animal;
+  
+//}
 
 function add2(n) {
   return n + two
