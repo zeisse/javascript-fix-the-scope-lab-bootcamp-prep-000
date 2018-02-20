66eb1a8267c8f6f882628850b3a8ab2b54bede61
Brook Zeisse <b@zeisse.com>
2018-02-20 00:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal = function yourAnimal () {
-    animal = cat;
+  return  function yourAnimal () {
+    var animal = cat;
     
   }
-    cat;
   return animal;
   
 }
