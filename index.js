ec0232421a1af8b24a48f8d085077de7c3a44b06
Brook Zeisse <b@zeisse.com>
2018-02-20 00:30:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal = yourAnimal('cat');
+  var animal = 'cat';
 }
 
 function add2(n) {
