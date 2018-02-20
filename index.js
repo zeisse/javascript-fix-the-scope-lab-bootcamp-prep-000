6e04a6e241727077ec9f17b7216b790fc9f3a1a4
Brook Zeisse <b@zeisse.com>
2018-02-20 00:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   var shit = yourAnimal
-  return yourAnimal('cat');
+  
 }
 
 function add2(n) {
