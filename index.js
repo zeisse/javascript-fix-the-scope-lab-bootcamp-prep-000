8e69d809dc0445dda75352f543694eac46fb83d7
Brook Zeisse <b@zeisse.com>
2018-02-20 00:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
     }
-  var myScope = yourAnimal();
-  myScope();
+  var animal = 'cat';
   
 }
 
